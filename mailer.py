b27372db503dcc0ee40297dca0c4b0db50ddaea3
DNibs <dnibs513@gmail.com>
2021-04-30 14:11:05 -0400

    removed cy305 elements to generalize

--- mailer.py
+++ mailer.py
@@ -3,26 +3,22 @@ Author: MAJ Crosser @ 5019
 
 Dependencies
     - pywin32
-        - from powershell (may require admin poweshell)
+        - from powershell (may require admin powershell)
         - pip install pywin32
     - Text file containing the message for the body of the email in same dir
         - "email_body_message.txt"
-    - CSV file of students names and email addresses
-        - "CY305_Cadet_Roster.csv"
+    - Text file containing the subject of the email in same dir
+        - "email_subject.txt"
+    - CSV file linking files to email addresses. Reads file name up to the underscore for dictionary key
+        - "roster.csv"
         
-Environment
-    - sync CY305 folder from SharePoint
-    - navigate to "\ay202\graded_events\database_project\output"
-    - run this script from your local copy
-    
 Usage
-    from PowerShell
-    py mailer.py all | [sec1] [sec2] ...
-    e.g.
-    > py mailer.py J25 K25
-    
+    - change directory below to path of files you wish to email
+    - place an "email_body_message.txt", "email_subject.txt", and "roster.csv" files in the director
+    - execute script
+
 Output
-    - searches directories and emails files to cadets (prints msg for each)
+    - emails files to cadets (prints msg for each)
     - Moves any emailed files to a folder named "emailed"    
 """
 
@@ -32,11 +28,12 @@ from os import listdir, makedirs, rename, remove, getcwd, walk
 import csv
 import sys
 
-EMAIL_BODY_FILE_NAME = getcwd()+'\\admin\\email_body_message.txt'
-CLASS_EMAIL_ROSTER_FILE_NAME = getcwd()+'\\admin\\roster.csv'
-EMAIL_SUBJECT = "Test Mail"
-USAGE = "usage: py mailer.py all | sec1 sec2 ..."
-OUTPUT_DIR = "output"
+
+# Change the directory variable
+DIRECTORY = 'C:\\Users\\david.niblick\\PycharmProjects\\mailer\\output\\'
+EMAIL_BODY_FILE_NAME = DIRECTORY + 'email_body_message.txt'
+CLASS_EMAIL_ROSTER_FILE_NAME = DIRECTORY + 'roster.csv'
+EMAIL_SUBJECT_FILE_NAME = DIRECTORY + "email_subject.txt"
 
 
 def send_email(recip, body, subject, att_rel_path):
@@ -57,62 +54,50 @@ def get_email_dict():
     with open(CLASS_EMAIL_ROSTER_FILE_NAME) as cSVHandle:
         email_reader = csv.reader(cSVHandle)
         for row in list(email_reader)[1:]:
-            last = row[0]
-            first = row[1]
-            email_dict[last + '.' + first] = row[2]
+            key = row[0]
+            email_dict[key] = row[1]
     return email_dict
 
 
-if len(sys.argv) < 2:
-    print(USAGE)
-    sys.exit()
-
 print(CLASS_EMAIL_ROSTER_FILE_NAME)
 ed = get_email_dict()
-dir_list = [x[0] for x in walk(getcwd()) if OUTPUT_DIR in x[0]]
 
-print(dir_list)
 print(ed)
 total_files = 0
 file_count = 0
-for directory in dir_list:
-    if "emailed" in directory:
-        continue
-    print('checking directory:', directory)
-    found_dir = False
-    if sys.argv[1].lower() != 'all':
-        for subdir in sys.argv[1:]:
-            if subdir in directory:
-                found_dir = True
-    else:
-        found_dir = True
-    if not found_dir:
-        continue
-    print('Good directory:', directory)
-    
-    makedirs(directory + '/' + 'emailed', exist_ok=True)
 
-    with open(EMAIL_BODY_FILE_NAME) as body_handle:
-        body_msg = body_handle.read()
 
-    files = [item for item in listdir(directory) if item.endswith('.txt')]
-    total_files += len(files)
-    
-    for file_name in files:
+makedirs(DIRECTORY + '/' + 'emailed', exist_ok=True)
+
+with open(EMAIL_BODY_FILE_NAME) as body_handle:
+    body_msg = body_handle.read()
+
+with open(EMAIL_SUBJECT_FILE_NAME) as body_handle:
+    email_subject = body_handle.read()
+
+if body_msg == '' or email_subject == '':
+    print('Missing valid email body from email_body_message.txt or subject from email_subject.txt')
+    exit()
+
+files = [item for item in listdir(DIRECTORY) if item.endswith('.txt')]
+total_files += len(files) - 2
+
+for file_name in files:
+    if file_name != 'roster.csv' and file_name != 'email_body_message.txt' and file_name != 'email_subject.txt':
         print("file_name is ", file_name)
         username = file_name.split('_')[0]
         print(username)
         print(f"Emailing {file_name} to {ed[username]}")
-        send_email(ed[username], 
-                   body_msg, 
-                   EMAIL_SUBJECT, 
-                   directory + '/' + file_name)
+        send_email(ed[username],
+                   body_msg,
+                   email_subject,
+                   DIRECTORY + '/' + file_name)
         file_count += 1
         try:
-            rename(directory + '/' + file_name,
-                   directory + '/emailed/' + file_name)
+            rename(DIRECTORY + '/' + file_name,
+                   DIRECTORY + '/emailed/' + file_name)
         except FileExistsError:
-            remove(directory + '/' + file_name)
+            remove(DIRECTORY + '/' + file_name)
 
 print("total files: ", total_files)
 print("total emails: ", file_count)
